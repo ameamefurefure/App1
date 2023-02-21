056d64adf0f4855f0d456f93cc94d9a64ded9baa
ameamefurefure <124484020+ameamefurefure@users.noreply.github.com>
2023-02-21 14:45:27 +0000

    7章まで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
     # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     #1.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -18,25 +18,31 @@ class ListsController < ApplicationController
     # 詳細画面へリダイレクト
     redirect_to list_path(list. id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
   
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を１件取得 
+    list.destroy #データ（レコード）を削除 
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
+  end
+
   private
   # ストロングパラメータ
   def list_params
